0da74f9c988ee461c7bf34e9a5d68eac6c050c83
Dhaval Pandey <dhavalpandey2007@outlook.com>
2022-03-22 17:36:35 +0000

    first commit

---
+++ public/assets/main.js
@@ -0,0 +1,36 @@
+var input = document.querySelector("#url");
+
+function isUrl(val = "") {
+  if (
+    /^http(s?):\/\//.test(val) ||
+    (val.includes(".") && val.substr(0, 1) !== " ")
+  )
+    return true;
+  return false;
+}
+
+input.addEventListener("keyup", (key) => {
+  if (key.keyCode == 13) {
+    if (!input.value.trim().length) return;
+    let isInputURL = isUrl(input.value);
+    let url = btoa(input.value);
+    if (isInputURL) {
+      window.location.assign(`/prox/?url=${url}`);
+    } else {
+      let googleSearch = btoa(`https://www.google.com/search?q=${input.value}`);
+      window.location.assign(`/prox/?url=${googleSearch}`);
+    }
+  }
+});
+
+document.querySelector("#initiate").addEventListener("click", () => {
+  if (!input.value.trim().length) return;
+  let isInputURL = isUrl(input.value);
+  let url = btoa(input.value);
+  if (isInputURL) {
+    window.location.assign(`/prox/?url=${url}`);
+  } else {
+    let googleSearch = btoa(`https://www.google.com/search?q=${input.value}`);
+    window.location.assign(`/prox/?url=${googleSearch}`);
+  }
+});
